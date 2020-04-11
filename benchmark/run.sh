f3209cd0184c649a8837dbf0165dedfe0458fbc6
Milton Silva <misilva@ubiwhere.com>
2020-04-11 15:25:58 +0100

    Update run.sh paramenters (higher compression)

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -9,10 +9,10 @@ cd ../src && make && cp -f AC2 ../benchmark && cd ../benchmark
 ./AC2 -l 4 -lr 0.4 -hs 40 ../benchmark/AminoAcidsCorpus/DA
 ./AC2 -l 4 -lr 0.3 -hs 40 ../benchmark/AminoAcidsCorpus/MJ
 ./AC2 -l 4 -lr 0.5 -hs 40 ../benchmark/AminoAcidsCorpus/HI
-./AC2 -l 5 -lr 0.4 -hs 40 ../benchmark/AminoAcidsCorpus/SA
-./AC2 -l 5 -lr 0.4 -hs 40 ../benchmark/AminoAcidsCorpus/LC
-./AC2 -l 5 -lr 0.4 -hs 40 ../benchmark/AminoAcidsCorpus/EC
-./AC2 -l 7 -lr 0.17 -hs 40 ../benchmark/AminoAcidsCorpus/HT
-./AC2 -l 6 -lr 0.17 -hs 40 ../benchmark/AminoAcidsCorpus/SC
-./AC2 -l 7 -lr 0.17 -hs 40 ../benchmark/AminoAcidsCorpus/HS
-./AC2 -l 7 -lr 0.16 -hs 40 ../benchmark/AminoAcidsCorpus/BT
+./AC2 -l 5 -lr 0.4 -hs 48 ../benchmark/AminoAcidsCorpus/SA
+./AC2 -l 5 -lr 0.4 -hs 64 ../benchmark/AminoAcidsCorpus/LC
+./AC2 -l 5 -lr 0.4 -hs 64 ../benchmark/AminoAcidsCorpus/EC
+./AC2 -l 7 -lr 0.17 -hs 64 ../benchmark/AminoAcidsCorpus/HT
+./AC2 -l 6 -lr 0.17 -hs 64 ../benchmark/AminoAcidsCorpus/SC
+./AC2 -l 7 -lr 0.17 -hs 64 ../benchmark/AminoAcidsCorpus/HS
+./AC2 -l 7 -lr 0.16 -hs 64 ../benchmark/AminoAcidsCorpus/BT
