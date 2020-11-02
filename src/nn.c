7a16a70ecde21b180999d911bbb4ebb080eaad49
Milton Silva <miltondts@gmail.com>
2020-11-02 19:09:24 +0000

    Try to hint the compiler to use packed instructions

--- src/nn.c
+++ src/nn.c
@@ -8,16 +8,16 @@
 
 static inline float Q_rsqrt( float number )
 {
-	const float x2 = number * 0.5F;
-	const float threehalfs = 1.5F;
+  const float x2 = number * 0.5F;
+  const float threehalfs = 1.5F;
 
-	union {
-		float f;
-		unsigned long i;
-	} conv  = { .f = number };
-	conv.i  = 0x5f3759df - ( conv.i >> 1 );
-	conv.f  *= ( threehalfs - ( x2 * conv.f * conv.f ) );
-	return conv.f;
+  union {
+    float f;
+    unsigned long i;
+  } conv  = { .f = number };
+  conv.i  = 0x5f3759df - ( conv.i >> 1 );
+  conv.f  *= ( threehalfs - ( x2 * conv.f * conv.f ) );
+  return conv.f;
 }
 
 
@@ -98,9 +98,6 @@ const float b2 = 0.9999;
 const float nb2 = 1 - b2;
 const float eps = 1E-8;
 
-//float bt1 = b1;
-//float bt2 = b2;
-
 void ann_train(ann_t *ann, float *t, float learning_rate) {
 
   float *m = ann->m;
@@ -111,66 +108,80 @@ void ann_train(ann_t *ann, float *t, float learning_rate) {
   const uint32_t ys = ann->ys;
 
   float d1[ys];
+  float vd1[ys];
+  float md1[ys];
   for(i = 0; i < ys; ++i) {
     d1[i] = ann->y[i] * (1 - ann->y[i]) * (t[i] - ann->y[i]);
+    vd1[i] = nb1 * d1[i];
+    md1[i] = nb2 * d1[i] * d1[i];
   }
 
   float *w2 = ann->why;
   float d2[hs];
+  float vd2[hs];
+  float md2[hs];
   for(i = 0; i < hs; ++i) {
+    for(j = 0; j < ys; ++j) {
+      *m = (b1 * *m) + (ann->h[i] * vd1[j]);
+      m++;
+    }
+    m -= ys;
+    const float hi2 = ann->h[i] * ann->h[i];
+    for(j = 0; j < ys; ++j) {
+      *v = (b2 * *v) + (hi2 * md1[j]);
+      v++;
+    }
+    v -= ys;
     d2[i] = 0;
-    const float hi = ann->h[i];
-
     for(j = 0; j < ys; ++j) {
       d2[i] += *w2 * d1[j];
-
-      *m = (b1 * *m) + (nb1 * hi * d1[j]);
-      *v = (b2 * *v) + (nb2 * hi * d1[j] * hi * d1[j]);
-
-      //float mhat = *m / (1 - bt1);
-      //float vhat = *v / (1 - bt2);
-
-      //*w2++ += learning_rate * mhat * Q_rsqrt(vhat + eps);
-	*w2++ += learning_rate * *m * Q_rsqrt(*v + eps);
+      *w2++ += learning_rate * *m * Q_rsqrt(*v + eps);
       m++;
       v++;
     }
+    d2[i] *= ann->h[i] * (1 - ann->h[i]);
+    vd2[i] = nb1 * d2[i];
+    md2[i] = nb2 * d2[i] * d2[i];
     ann->h[i] = 0;
-    d2[i] *= hi * (1 - hi);
   }
 
-  for(i = 0; i < ys; ++i) {
-    *m = (b1 * *m) + (nb1 * d1[i]);
-    *v = (b2 * *v) + (nb2 * d1[i] * d1[i]);
+  
 
-    //float mhat = *m / (1 - bt1);
-    //float vhat = *v / (1 - bt2);
-    //*w2++ +=  learning_rate * mhat * Q_rsqrt(vhat + eps);
-    *w2++ += learning_rate * *m * Q_rsqrt(*v + eps);
+  for(i = 0; i < ys; ++i) {
+    *m = (b1 * *m) + vd1[i];
+    m++;
     ann->y[i] = 0;
+  }
+  m -= ys;
+  for(i = 0; i < ys; ++i) {
+    *v = (b2 * *v) + md1[i];
+    v++;
+  }
+  v -= ys;
+  for(i = 0; i < ys; ++i) {
+    *w2++ += learning_rate * *m * Q_rsqrt(*v + eps);
     m++;
     v++;
   }
 
   float *w1 = ann->wxh;
   for(i = 0; i < xs; ++i) {
-    const float nxi1 = nb1 * ann->x[i];
-    const float nxi2 = nb2 * ann->x[i] * ann->x[i];
     for(j = 0; j < hs; ++j) {
-      *m = (b1 * *m) + (nxi1 * d2[j]);
-      *v = (b2 * *v) + (nxi2 * d2[j] * d2[j]);
-
-      //float mhat = *m / (1 - bt1);
-      //float vhat = *v / (1 - bt2);
-      //*w1++ +=  learning_rate * mhat * Q_rsqrt(vhat + eps);
-	*w1++ +=  learning_rate * *m * Q_rsqrt(*v + eps);
-      //printf("%f\n", (learning_rate) / (sqrt(vhat) + eps));
+      *m = (b1 * *m) + (ann->x[i] * vd2[j]);
+      m++;
+    }
+    m -= hs;
+    const float xi2 = ann->x[i] * ann->x[i];
+    for(j = 0; j < hs; ++j) {
+      *v = (b2 * *v) + (xi2 * md2[j]);
+      v++;
+    }
+    v -= hs;
+    for(j = 0; j < hs; ++j) {
+      *w1++ += learning_rate * *m * Q_rsqrt(*v + eps);
       m++;
       v++;
     }
   }
-
-  //bt1 *= bt1;
-  //bt2 *= bt2;
 }
 
